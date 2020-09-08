0f70aae678e64db7a4bccce8e26040369e949cf1
Moises Sanabria <m@moises.tech>
2020-09-08 08:22:55 -0500

    entry, putput, path, filename, publicPath, module, mode, plugins, devServer

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "index_bundle",
+    filename: "index_bundle.js",
     publicPath: "/",
   },
   module: {
