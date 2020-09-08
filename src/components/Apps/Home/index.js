65bc1624f135bd1288aa718a4976a4900b4a61aa
Moises Sanabria <m@moises.tech>
2020-09-08 08:37:49 -0500

    func comp for home and 404 routing

---
+++ src/components/Apps/Home/index.js
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export default function Home() {
+  return (
+    <div className="app-body">
+      <h1>Click an Arcade App!</h1>
+    </div>
+  );
+}
---
+++ src/components/Apps/NotFound/index.js
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export default function NotFound() {
+  return (
+    <div className="app-body">
+      <h1>Click an Arcade App!</h1>
+    </div>
+  );
+}
