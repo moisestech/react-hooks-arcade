0395d3f9d9293b28e4eb5a23f8f34d7ff462b71d
Moises Sanabria <m@moises.tech>
2020-09-11 11:49:02 -0500

    CalDatesArray, CalPicker, Date Choose Buttons and Cal

--- src/components/Apps/CalendarPicker/index.js
+++ src/components/Apps/CalendarPicker/index.js
@@ -1,10 +1,29 @@
 import * as React from "react";
 import "./index.css";
 
+const calendarDates = Array(31)
+  .fill(0)
+  .map((e, i) => i);
+
 export default function CalendarPicker() {
   return (
     <div className="calendar-picker app-body">
       <h1>Calendar Picker</h1>
+      <div className="date-chooser">
+        <button className="date-chooser-button">
+          Start Date <span>0</span>
+        </button>
+
+        <button className="date-chooser-button">
+          End Date <span>0</span>
+        </button>
+      </div>
+
+      <div className="calendar">
+        {calendarDates.map((day, index) => {
+          return <div className="calendar-day">{day + 1}</div>;
+        })}
+      </div>
     </div>
   );
 }
