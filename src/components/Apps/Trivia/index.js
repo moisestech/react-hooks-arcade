6fd6a98583c2d438609fa3619719f05dd9b7fca8
Moises Sanabria <m@moises.tech>
2020-09-15 20:40:07 -0500

    init App Tree, Todo, Learn, Questions

--- src/components/Apps/Trivia/index.js
+++ src/components/Apps/Trivia/index.js
@@ -1,10 +1,31 @@
 import * as React from "react";
 import "./index.css";
+import Question from "./components/Question";
+import CategorySelector from "./components/CategorySelector";
+import ResultsModal from "./components/ResultModal";
+import ScoreBoard from "./components/ScoreBoard";
 
 export default function Trivia() {
   return (
     <div className="trivia app-body">
-      <h1>Trivia</h1>
+      {/* Show the result modela */}
+      {/* <ResultModal /> */}
+
+      {/* Question Header */}
+      <div className="question-header">
+        <CategorySelector />
+        <ScoreBoard />
+      </div>
+
+      {/* Question itself */}
+      <div className="question-main">
+        <Question />
+      </div>
+
+      {/* Question Footer */}
+      <div className="question-footer">
+        <button>Go to next question 👉</button>
+      </div>
     </div>
   );
 }
