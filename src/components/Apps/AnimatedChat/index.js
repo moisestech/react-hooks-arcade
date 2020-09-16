4079218d985543b0dce39283082f28908d5e327e
Moises Sanabria <m@moises.tech>
2020-09-16 08:42:11 -0500

    initi Anim Template react func comp

---
+++ src/components/Apps/AnimatedChat/index.js
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export default function AnimatedChat() {
+  return (
+    <div className="animated-chat app-body">
+      <h1>Animated Chat</h1>
+    </div>
+  );
+}
