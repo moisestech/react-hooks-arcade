77fc6e65a47de3bbbcd2e5d0f92d3dcebef531f1
Moises Sanabria <m@moises.tech>
2020-09-16 11:54:57 -0500

    openweathermap api, ChartWeather func comp

---
+++ src/components/Apps/ChartWeather/index.js
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+// openweathermap.org
+// https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,minutely&units=imperial&appid=bb96c7f9ac6f57dc00333727c5407547
+// api key: bb96c7f9ac6f57dc00333727c5407547
+
+// google maps api
+// api key: AIzaSyDp4os33WF6-4d-xFVyL0HsUUHN7dOml_w
+
+export default function ChartWeather() {
+  return (
+    <div className="chart-weather app-body">
+      <h1>Chart Weather</h1>
+      {/* form goes here */}
+      {/* chart goes here */}
+    </div>
+  );
+}
