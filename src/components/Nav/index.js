ba6518d6b0496d1c2221775c36aeffeb26d672dd
Moises Sanabria <m@moises.tech>
2020-09-17 22:15:02 -0500

    tag default "App Arcade"

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,28 +1,33 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import AppBtnInfo from "./components/AppBtnInfo";
+import NavBottom from "./components/NavBottom";
 import NavBarArray from "./utils/navbar-array";
 
 export default function Nav() {
   return (
     <div className="navbar">
-      <NavLink exact to="/" activeClassName="is-active">
-        <h1>React Hooks Arcade</h1>
-        <span className="app-list-header">Interactive Apps</span>
-      </NavLink>
+      <div className="navleft">
+        <NavLink exact to="/" activeClassName="is-active">
+          <h1>React Arcade</h1>
+          <span className="app-list-header">Interactive Apps</span>
+        </NavLink>
 
-      {NavBarArray.map((nav, index) => {
-        return (
-          <NavLink
-            key={index}
-            to={nav.to}
-            className={nav.className}
-            activeClassName="is-active"
-          >
-            <AppBtnInfo icon={nav.icon} title={nav.title} info={nav.info} />
-          </NavLink>
-        );
-      })}
+        {NavBarArray.map((nav, index) => {
+          return (
+            <NavLink
+              key={index}
+              to={nav.to}
+              className={nav.className}
+              activeClassName="is-active"
+            >
+              <AppBtnInfo icon={nav.icon} title={nav.title} info={nav.info} />
+            </NavLink>
+          );
+        })}
+      </div>
+
+      <NavBottom />
     </div>
   );
 }
