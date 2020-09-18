8afdd07830ceb001a9fa83170af2519f81e87866
Moises Sanabria <m@moises.tech>
2020-09-17 22:16:33 -0500

    carousel comp next to NavBarArray.map(nav AppBtnInfo

--- src/components/Pages/Home/index.js
+++ src/components/Pages/Home/index.js
@@ -1,9 +1,29 @@
 import * as React from "react";
+import { NavLink } from "react-router-dom";
+import AppBtnInfo from "../../Nav/components/AppBtnInfo";
+import NavBarArray from "../../Nav/utils/navbar-array";
+import Carousel from "./components/Carousel";
 
 export default function Home() {
   return (
     <div className="app-body">
-      <h1>Click an Arcade App!</h1>
+      <Carousel />
+      <ul className="home-feed">
+        {NavBarArray.map((nav, index) => {
+          return (
+            <li key={index}>
+              <NavLink
+                key={index}
+                to={nav.to}
+                className={nav.className}
+                activeClassName="is-active"
+              >
+                <AppBtnInfo icon={nav.icon} title={nav.title} info={nav.info} />
+              </NavLink>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
