92b521fbfb815d94d5261eaab2d1e488ad8d2b64
Moises Sanabria <m@moises.tech>
2020-09-18 17:16:36 -0500

    arg path "/" ? {tag} = "App Arcade"

--- src/components/Nav/components/AppBtnInfo/index.js
+++ src/components/Nav/components/AppBtnInfo/index.js
@@ -1,16 +1,22 @@
 import * as React from "react";
 import PropTypes from "prop-types";
 
-export default function AppBtnInfo({ icon, title, info, tag = "App Arcade" }) {
+export default function AppBtnInfo({
+  icon,
+  title,
+  info,
+  tag = "App Arcade",
+  path,
+}) {
   return (
-    <>
+    <div className="app-btn-info">
       <span className="icon">{icon}</span>
       <ul className="nav-app-info">
-        <li className="tag">{tag}</li>
+        {path === "/" ? <li className="tag">{tag}</li> : null}
         <li className="title">{title}</li>
         <li className="info">{info}</li>
       </ul>
-    </>
+    </div>
   );
 }
 
