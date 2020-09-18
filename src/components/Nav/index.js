c26860be6555ce5573b8aa5aa4a86472613924d6
Moises Sanabria <m@moises.tech>
2020-09-18 17:17:51 -0500

    useLoaction {pathname} = "/" ? null : <Nvlft/>, NvBotm>

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,32 +1,15 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
-import AppBtnInfo from "./components/AppBtnInfo";
+import { useLocation } from "react-router-dom";
 import NavBottom from "./components/NavBottom";
-import NavBarArray from "./utils/navbar-array";
+import NavLeft from "./components/NavLeft";
 
 export default function Nav() {
+  const { pathname } = useLocation();
+  console.log(pathname, "location");
+
   return (
     <div className="navbar">
-      <div className="navleft">
-        <NavLink exact to="/" activeClassName="is-active">
-          <h1>React Arcade</h1>
-          <span className="app-list-header">Interactive Apps</span>
-        </NavLink>
-
-        {NavBarArray.map((nav, index) => {
-          return (
-            <NavLink
-              key={index}
-              to={nav.to}
-              className={nav.className}
-              activeClassName="is-active"
-            >
-              <AppBtnInfo icon={nav.icon} title={nav.title} info={nav.info} />
-            </NavLink>
-          );
-        })}
-      </div>
-
+      {pathname === "/" ? null : <NavLeft path={pathname} />}
       <NavBottom />
     </div>
   );
