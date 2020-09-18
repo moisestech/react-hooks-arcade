057e4af21c7fe7d9af12b6a4270642d777b029e3
Moises Sanabria <m@moises.tech>
2020-09-17 22:32:15 -0500

    init .app-body func comp RealTimeSearch

---
+++ src/components/Apps/RealTimeSearch/index.js
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export default function RealTimeSearch() {
+  return (
+    <div className="realtime-search">
+      <div className="search-container">Real Time Search</div>
+    </div>
+  );
+}
