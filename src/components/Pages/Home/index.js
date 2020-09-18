82d29c6bc55e95c3d1c2e3dee7972b5652e402e8
Moises Sanabria <m@moises.tech>
2020-09-18 17:19:26 -0500

    imp <Crsl> <AppFeed>

--- src/components/Pages/Home/index.js
+++ src/components/Pages/Home/index.js
@@ -1,29 +1,14 @@
 import * as React from "react";
-import { NavLink } from "react-router-dom";
-import AppBtnInfo from "../../Nav/components/AppBtnInfo";
-import NavBarArray from "../../Nav/utils/navbar-array";
 import Carousel from "./components/Carousel";
+import AppFeed from "./components/AppFeed";
 
 export default function Home() {
   return (
     <div className="app-body">
-      {/* <Carousel /> */}
-      <ul className="home-feed">
-        {NavBarArray.map((nav, index) => {
-          return (
-            <li key={index}>
-              <NavLink
-                key={index}
-                to={nav.to}
-                className={nav.className}
-                activeClassName="is-active"
-              >
-                <AppBtnInfo icon={nav.icon} title={nav.title} info={nav.info} />
-              </NavLink>
-            </li>
-          );
-        })}
-      </ul>
+      <div className="home-feed">
+        <Carousel />
+        <AppFeed />
+      </div>
     </div>
   );
 }
