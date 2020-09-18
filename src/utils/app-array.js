729602484e5ebd47cd6d8bbd6766937e60f2fd0a
Moises Sanabria <m@moises.tech>
2020-09-18 17:38:47 -0500

    app array w/ img src Desktop image -> <Home> <AppFeed>

---
+++ src/utils/app-array.js
@@ -0,0 +1,161 @@
+export default [
+  {
+    to: "/pomodoro",
+    className: "pomodoro",
+    title: "Pomodoro",
+    icon: "🕒",
+    info: "Dynamic Timer",
+    img:
+      "https://raw.githubusercontent.com/moisestech/react-hooks-arcade/master/src/components/Apps/Pomodoro/public/pomodoro.gif",
+  },
+  {
+    to: "/markdown-editor",
+    className: "markdown-editor",
+    title: "Markdown Editor",
+    icon: "💻",
+    info: "Text Editor",
+    img:
+      "https://raw.githubusercontent.com/moisestech/react-hooks-arcade/master/src/components/Apps/MarkdownEditor/public/markdown-editor.gif",
+  },
+  {
+    to: "/highlight-tabs",
+    className: "highlight-tabs",
+    title: "Highlight Tabs",
+    icon: "🎫",
+    info: "Web Browser",
+    img:
+      "https://raw.githubusercontent.com/moisestech/react-hooks-arcade/master/src/components/Apps/HighlightTabs/public/highlight-tabs.gif",
+  },
+  {
+    to: "/paper-rock-scissors",
+    className: "paper-rock-scissors",
+    title: "Paper Rock Scissors",
+    icon: "✂️",
+    info: "Classic Game",
+    img:
+      "https://raw.githubusercontent.com/moisestech/react-hooks-arcade/master/src/components/Apps/PaperRockScissors/public/paper-rock-scissors.gif",
+  },
+  {
+    to: "/adventure-link",
+    className: "adventure-link",
+    title: "Adventure Link",
+    icon: "⚔️",
+    info: "Open World Game",
+    img:
+      "https://raw.githubusercontent.com/moisestech/react-hooks-arcade/master/src/components/Apps/AdventureLink/public/adventure-link.gif",
+  },
+  {
+    to: "/infinite-image-gallery",
+    className: "infinite-image-gallery",
+    title: "Infinite Image Gallery",
+    icon: "🖼️",
+    info: "Photo Viewer",
+    img: "",
+  },
+  {
+    to: "/trivia",
+    className: "trivia",
+    title: "Trivia",
+    icon: "📖",
+    info: "Quiz and Knowledge",
+    img:
+      "https://raw.githubusercontent.com/moisestech/react-hooks-arcade/master/src/components/Apps/Trivia/public/trivia.gif",
+  },
+  {
+    to: "/authentication",
+    className: "authentication",
+    title: "Authentication",
+    icon: "🔒",
+    info: "Login Application",
+    img:
+      "https://raw.githubusercontent.com/moisestech/react-hooks-arcade/master/src/components/Apps/Authentication/public/images/gandalf.png",
+  },
+  {
+    to: "/web-speech-timers",
+    className: "web-speech-timers",
+    title: "Web Speech Timers",
+    icon: "💬",
+    info: "Chat and messaging",
+    img:
+      "https://raw.githubusercontent.com/moisestech/react-hooks-arcade/master/src/components/Apps/WebSpeechTimers/public/web-speech-timers.gif",
+  },
+  {
+    to: "/calendar-picker",
+    className: "calendar-picker",
+    title: "Calendar Picker",
+    icon: "🗓️",
+    info: "Scheduling & Time",
+    img:
+      "https://raw.githubusercontent.com/moisestech/react-hooks-arcade/master/src/components/Apps/CalendarPicker/public/calendar-picker.gif",
+  },
+  {
+    to: "/animated-chat",
+    className: "animated-chat",
+    title: "Animated Chat",
+    icon: "🤖",
+    info: "interactive, social",
+    img:
+      "https://raw.githubusercontent.com/moisestech/react-hooks-arcade/master/src/components/Apps/AnimatedChat/public/animated-chat.gif",
+  },
+  {
+    to: "/multiform",
+    className: "multiform",
+    title: "MultiForm",
+    icon: "📱",
+    info: "forms and input",
+    img:
+      "https://raw.githubusercontent.com/moisestech/react-hooks-arcade/master/src/components/Apps/MultiForm/public/multiform.gif",
+  },
+  {
+    to: "/memory-game",
+    className: "memory-game",
+    title: "Memory Game",
+    icon: "🃏",
+    info: "Card game",
+    img:
+      "https://raw.githubusercontent.com/moisestech/react-hooks-arcade/master/src/components/Apps/MemoryGame/public/memory-game.gif",
+  },
+  {
+    to: "/drag-drop-match",
+    className: "drag-drop-match",
+    title: "Drag Drop Match",
+    icon: "📦",
+    info: "card game",
+    img:
+      "https://raw.githubusercontent.com/moisestech/react-hooks-arcade/master/src/components/Apps/DragDropMatch/public/dragDropMatch.gif",
+  },
+  {
+    to: "/browser",
+    className: "browser",
+    title: "Web Browser",
+    icon: "🌐",
+    info: "Web surfing",
+    img:
+      "https://raw.githubusercontent.com/moisestech/react-hooks-arcade/master/src/components/Apps/Browser/public/browser-iframe.gif",
+  },
+  {
+    to: "/realtime-search",
+    className: "realtime-search",
+    title: "Real Time Search",
+    icon: "🔍",
+    info: "Search Engine",
+    img:
+      "https://raw.githubusercontent.com/moisestech/react-hooks-arcade/master/src/components/Apps/RealTimeSearch/public/realtime-search.gif",
+  },
+  {
+    to: "/chart-weather",
+    className: "chart-weather",
+    title: "Chart Weather",
+    icon: "☀️",
+    info: "Scheduling & Time",
+    img: "",
+  },
+  {
+    to: "/cartoon-social",
+    className: "cartoon-social",
+    title: "Cartoon Social",
+    icon: "✌️",
+    info: "Contact & Messaging",
+    img: "",
+  },
+];
