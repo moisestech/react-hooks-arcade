8882caba472f64a6ab8871a002c386925b336198
Moises Sanabria <m@moises.tech>
2020-09-17 22:30:09 -0500

    init func comp MultiForm .app-body

---
+++ src/components/Apps/MultiForm/index.js
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export default function MultiForm() {
+  return (
+    <div className="multiform app-body">
+      <div className="app">Signup form goes here</div>
+    </div>
+  );
+}
