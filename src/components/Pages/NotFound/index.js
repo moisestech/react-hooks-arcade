c414edc0d33fe9602ece27f6ed95c5752e8828c3
Moises Sanabria <m@moises.tech>
2020-09-17 22:17:21 -0500

    coming soon extendable css animation arg phrase

--- src/components/Pages/NotFound/index.js
+++ src/components/Pages/NotFound/index.js
@@ -1,9 +1,11 @@
 import * as React from "react";
+import TextAnim from "./components/TextAnim";
 
 export default function NotFound() {
   return (
     <div className="app-body">
-      <h1>Click an Arcade App!</h1>
+      {/* <h1>Click an Arcade App!</h1> */}
+      <TextAnim phrase={"App Coming Soon"} />
     </div>
   );
 }
