82b0a79159c05cdbb6d80d5cb2d6404cb3732795
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-23 21:49:40 -0400

    accessKey, getPhotos useCallback, fetch(apiURL) useEffect, getPhotos, searchPhotos, InfiniteScroll

--- src/components/Apps/InfiniteImageGallery/index.js
+++ src/components/Apps/InfiniteImageGallery/index.js
@@ -1,38 +1,104 @@
-import { useEffect } from "react";
+import { useEffect, useState, useCallback, useRef } from "react";
 import "./index.css";
+import InfiniteScroll from "react-infinite-scroll-component";
 
-const accessKey = process.env.REACT_APP_UNSPLASH_ACESS_KEY;
+// NOTE: make sure to add your unsplash api key
+// https: //unsplash.com/developers
+// check webpack config for setup
+const accessKey = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
 
 export default function InfiniteImageGallery() {
-  useEffect(() => {
-    fetch(
-      "https://api.unsplash.com/photos?client_id=6LvmmK4yxXY2uh6T_sTSF8XBMyxWnKs3qV7yV0s2050"
-    )
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState("");
+  const _previousKeyRef = useRef(null);
+  const _previousPage = useRef(null);
+
+  const getPhotos = useCallback(() => {
+    _previousKeyRef.current = query;
+
+    let apiURL = `https://api.unsplash.com/photos?`;
+    if (query) apiURL = `https://api.unsplash.com/search/photos?query=${query}`;
+
+    apiURL += `&page=${page}`;
+    apiURL += `&client_id=${accessKey}`;
+
+    fetch(apiURL)
       .then((res) => res.json())
-      .then((data) => {});
-  }, []);
+      .then((data) => {
+        console.log(data);
+        const imagesFromApi = data.results ?? data;
+
+        let mounted = true;
+        if (mounted) {
+          //  if page is 1, then we need a whole new array of images
+          if (page === 1) return setImages(imagesFromApi);
+
+          // if page > 1, then we are adding from our infinite scroll
+          setImages((images) => [...image, ...imagesFromApi]);
+        }
+      });
+
+    return () => (mounted = false);
+  }, [page, query]);
+
+  useEffect(() => {
+    getPhotos();
+  }, [page]);
+
+  function searchPhotos(e) {
+    e.preventDefault();
+    setPage(1);
+    getPhotos();
+  }
 
   // return an error if there is no access key
   if (!accessKey) {
-    return <a href="https://unsplash.com/documentation" />;
+    return (
+      <a className="error" href="https://unsplash.com/documentation">
+        Required: Get Your Unsplash API Key First
+      </a>
+    );
   }
 
   return (
     <div className="infinite-image-gallery app-body">
       <h1>Unplash Image Gallery</h1>
 
-      <form>
-        <input type="text" placeholder="Search Unsplash..." />
+      <form onSubmit={searchPhotos}>
+        <input
+          type="text"
+          placeholder="Search Unsplash..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
         <button>Search</button>
       </form>
 
-      <div className="image-grid">
-        {[...Array(100)].map((_, index) => (
-          <div className="image" key={index}>
-            <img src="https://placekitten.com/g/1920/1080" alt="Sample" />
-          </div>
-        ))}
-      </div>
+      <InfiniteScroll
+        dataLength={images.length}
+        next={() => {
+          if (_previousPage.current === page) {
+            setPage((page) => page + 1);
+          }
+        }}
+        hasMore={true}
+        loader={<h4>Loading...</h4>}
+      >
+        <div className="image-grid">
+          {images.map((image, index) => (
+            <a
+              className="image"
+              key={index}
+              href={image.links.html}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={image.urls.regular} alt={image.alt_description} />
+            </a>
+          ))}
+        </div>
+      </InfiniteScroll>
     </div>
   );
 }
