80bb6d0d57703f4ea9e95da27250bfbebed4a5a0
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-24 05:56:00 -0400

    dotenv, ProvidPlugin process/browser, DefinePlugin "process.env" JSON.stringify(dotenv.config().parsed)

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,11 @@
 const path = require("path");
+const webpack = require("webpack");
+const dotenv = require("dotenv");
+
+// const dotenv = require("dotenv").config({
+//   path: path.join(__dirname, ".env"),
+// });
+
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 
@@ -23,6 +30,12 @@ module.exports = {
     }),
     new CopyPlugin({
       patterns: [{ from: "./public/_redirects" }],
+    }),
+    new webpack.ProvidePlugin({
+      process: "process/browser",
+    }),
+    new webpack.DefinePlugin({
+      "process.env": JSON.stringify(dotenv.config().parsed),
     }),
   ],
   devServer: {
